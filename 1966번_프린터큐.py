a18ceb69e1ba3f13c88d74e3b27e578efe862e07
minjeoong <88662427+minjeoong@users.noreply.github.com>
2023-03-07 23:39:54 +0900

    3/7(화)

--- 1966번_프린터큐.py
+++ 1966번_프린터큐.py
@@ -1,8 +1,27 @@
+# b : 내위치 
+# cnt : 이동 횟수 
+from collections import deque
 n = int(input())
+
 for i in range(n):
     a, b = map(int, input().split())
-    lst = list(map(int, input().split()))
-    x = lst[b]
-    lst.sort(reverse=True)
-    print(lst.index(x)+1)
-    ############# 실패 이건 다시 풀ㄹ어야돼
+    deq = deque(list(map(int, input().split())))
+    cnt = 0
+    while deq:
+        Wang = max(deq)
+        front = deq.popleft()
+        b -= 1
+
+        if front == Wang:
+            cnt += 1
+            if b < 0:
+                print(cnt)
+                break
+        else:
+            deq.append(front)
+            if b < 0:
+                b = len(deq) - 1
+        
+
+    
+
