d668dbb7eae0d60d080c8412a1dd0ac622efa14a
minjeoong <88662427+minjeoong@users.noreply.github.com>
2023-03-08 23:34:17 +0900

    3/8(수)

---
+++ 24262번_알고리즘수업-알고리즘의수행시간1.py
@@ -0,0 +1,13 @@
+# def MenOfPassion(A[], n){
+# 	i = ⌊n / 2⌋;
+#     return A[i];
+# }
+# 이 코드는 입력값과 상관없이 딱 한번 a 배열의 특정 인덱스를 조회해 리턴하므로 
+# 1 : 수행 횟수
+# 0 : 수행횟수를 다항식으로 냐ㅏ타냈을 때 최고차항의 차수 
+
+import sys
+
+n = sys.stdin.readline()
+print(1)
+print(0)
