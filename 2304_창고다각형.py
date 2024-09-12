352b11528ee6d2f1c26d09a798677d24eba44b9d
minjeoong <orange_mj@naver.com>
2024-09-12 23:56:32 +0900

    24.09.11(수)

--- 2304_창고다각형.py
+++ 2304_창고다각형.py
@@ -1,3 +1,6 @@
+# print("i = %d -> %d * (%d - %d)" % (i, height, lst[i][0], lst[i-1][0]))
+
+
 n = int(input())
 lst = []
 result = 0
@@ -8,31 +11,32 @@ for _ in range(n):
 
 lst.sort()
 
-# 가장 높은 기둥의 높이
+# 가장 높은 기둥의 높이, 그 높이를 result 에 합산
 for i in range(n):
   if lst[i][1] > result:
     result = lst[i][1]
     idx = i
 
+
+# 첫 높이 세팅은 가장 왼쪽의 높이
 height = lst[0][1]
+
 # 왼쪽에서 가장 높은 기둥까지의 넓이
 for i in range(idx):
     if height < lst[i+1][1]:
       result += height * (lst[i+1][0] - lst[i][0])
-      print("i = %d -> %d * (%d - %d)" % (i, height, lst[i+1][0], lst[i][0]))
       height = lst[i+1][1]
     else: 
       result += height * (lst[i+1][0] - lst[i][0])
-      print("i = %d -> %d * (%d - %d)" % (i, height, lst[i+1][0], lst[i][0]))
 
-# 오른쪽에서 가장 높은 기둥까지의 높이
+# 가장 오른쪽의 높이로 세팅
 height = lst[-1][1]
+
+# 오른쪽에서 가장 높은 기둥까지의 넓이
 for i in range(n-1, idx, -1):
   if height < lst[i-1][1]:
       result += height * (lst[i][0]-lst[i-1][0])
-      print("i = %d -> %d * (%d - %d)" % (i, height, lst[i][0], lst[i-1][0]))
       height = lst[i-1][1]
   else:
       result += height * (lst[i][0] - lst[i-1][0])
-      print("i = %d -> %d * (%d - %d)" % (i, height, lst[i][0], lst[i-1][0]))
 print(result)
