333407f37864177253c29113326e0fddc271eb2e
minjeoong <orange_mj@naver.com>
2024-10-08 02:59:44 +0900

    24.10.07(월)

---
+++ 24416_알고리즘수업-피보나치수1.py
@@ -0,0 +1,28 @@
+n = int(input())
+global code1, code2
+f = [0] * (n + 1)
+
+
+def fib(n):
+    global code1
+    if n == 1 or n == 2:
+      code1 += 1; 
+      return 1;  # 코드1
+    else:
+      return (fib(n - 1) + fib(n - 2))
+
+
+def fibonacci(n):
+    global code2
+    f[1] = 1
+    f[2] = 1
+    for i in range(3,n+1):
+        code2 += 1
+        f[i] = f[i - 1] + f[i - 2] # 코드2
+    return f[n]
+
+code1 = 0
+code2 = 0
+fib(n)
+fibonacci(n)
+print(code1, code2)
--- 9084_동전.py
+++ 9084_동전.py
@@ -1,3 +1,8 @@
+# https://d-cron.tistory.com/23 참고
+# 2차원으로 해결하는 법 선택했고, 1차원으로 해결도 가능한 문제
+# dp 를 활용 
+
+
 import sys
 t = int(sys.stdin.readline())
 
@@ -18,7 +23,7 @@ for i in range(t):
 
   for a in range(1, n+1):
     for b in range(1, m+1):
-      dp[a][b] = dp[a-1][b]
-      if b-cost_lst[a] >= 0:
-        dp[a][b] += dp[a][b-cost_lst[a]]
+      dp[a][b] = dp[a-1][b] # 그 전에 있던 행을 복제 (2원으로 만들수 있는 가지수 + 다음 돈으로 만들 수 있는 갓지수 ... 다 합해야하기 때문)
+      if b-cost_lst[a] >= 0: # X-2 에 있는 값을 가져와서 더해주는 로직인데. X-2 index 가 0보다 작으면 안되기 때문
+        dp[a][b] += dp[a][b-cost_lst[a]] # 같은 행에 X-2 에 있는 값을 가져와서 현재 값과 더해줌 
   print(dp[n][m])
