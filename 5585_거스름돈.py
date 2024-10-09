b5b43745a76be68caef2f971dea32e1e346b4ab7
minjeoong <orange_mj@naver.com>
2024-10-10 05:02:56 +0900

    24.10.09(수)

--- 5585_거스름돈.py
+++ 5585_거스름돈.py
@@ -1,10 +1,19 @@
 
 
-n = int(input())
+num = 1000-int(input())
 
-for i in range(n):
-  num = int(input())
-  
-  for j in range(num):
-    print("=",end="")
-  print()
+money = [500, 100, 50, 10, 5, 1]
+cnt = 0
+
+while True:
+
+  if num == 0:
+    break
+    
+  for i in money:
+    if num >= i:
+      num -= i
+      cnt += 1
+      break
+    
+print(cnt)
